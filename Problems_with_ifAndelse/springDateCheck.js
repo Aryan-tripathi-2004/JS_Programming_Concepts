e2734ff36a9519669804a66e85f0988821326563
Om Datt Tripathi <aryantripathi1606@gmail.com>
2025-03-18 22:17:33 +0530

    Revert "Write a program that takes day and month from the command line and prints true if"
    This reverts commit 65e5a080667430d96281a4c2ba36ba12e277d3ba.

--- Problems_with_ifAndelse/springDateCheck.js
+++
@@ -1,9 +0,0 @@
-const day = parseInt(process.argv[2]);
-const month = parseInt(process.argv[3]);
-
-const isValid =
-  (month === 3 && day >= 20) ||
-  (month > 3 && month < 6) ||
-  (month === 6 && day <= 20);
-
-console.log(isValid);
